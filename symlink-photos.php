8d2c899d35aea33c911129b3d722da2b3ff85945
Kaspars <hi@kaspars.net>
2015-12-26 13:09:40 +0200

    Add the symlinker

---
+++ symlink-photos.php
@@ -0,0 +1,46 @@
+<?php
+
+date_default_timezone_set( exec( 'date +%Z' ) );
+
+$photos_dir = $_SERVER['HOME'] . '/Pictures/Photos Library.photoslibrary';
+$photos_library = $photos_dir . '/database/Library.apdb';
+$photos_library_local = dirname( __FILE__ ) . '/Library.apdb';
+$links_dir = dirname( __FILE__ ) . '/links';
+
+// Move it here to avoid locking and permission issues
+if ( file_exists( $photos_library_local ) )
+	unlink( $photos_library_local );
+
+copy( $photos_library, $photos_library_local );
+
+$db = new PDO( 'sqlite:' . $photos_library_local );
+
+$query = $db->query(
+	'SELECT
+		imagePath as path,
+		( imageDate + 978307200 ) as timestamp
+	FROM
+		RKMaster
+	'
+);
+
+$photos = $query->fetchAll( PDO::FETCH_ASSOC );
+
+foreach ( $photos as $photo ) {
+
+	$target = sprintf( '%s/Masters/%s', $photos_dir, $photo['path'] );
+	$link_folder = sprintf( '%s/%s', $links_dir, date( 'Y-m', $photo['timestamp'] ) );
+	$link = sprintf( '%s/%s-%s', $link_folder, md5( $photo['path'] ), basename( $photo['path'] ) );
+
+	if ( ! file_exists( $target ) )
+		continue;
+
+	if ( ! is_dir( $link_folder ) )
+		mkdir( $link_folder );
+
+	if ( ! file_exists( $link ) )
+		symlink( $target, $link );
+
+}
+
+unlink( $photos_library_local );
